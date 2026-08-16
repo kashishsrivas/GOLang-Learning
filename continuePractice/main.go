ae7802333fb799d5a6249895dfb28fa3771e444e
kashish srivastava <kashishsrivastava.delhi@gmail.com>
2026-08-16 23:28:12 +0530

    Update continuePractice main.go

--- continuePractice/main.go
+++ continuePractice/main.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func main() {
 	//Print all numbers from 1 to 20, except the numbers divisible by 3.
-	for i := 1; i <= 20; i++ {
-		if i%3 == 0 {
+	// for i := 1; i <= 20; i++ {
+	// 	if i%3 == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	//sum of all numbers from 1 to n that are NOT divisible by 3.
+	n := 20
+	for i := 1; i <= n; i++ {
+		if i%3 != 0 {
 			continue
 		}
 		fmt.Println(i)
