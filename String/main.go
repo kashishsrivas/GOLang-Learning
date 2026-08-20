9ba76067eee15619e8c4a416048105038f3b2525
kashish srivastava <120769796+kashishsrivas@users.noreply.github.com>
2026-08-20 23:26:02 +0530

    Add files via upload

---
+++ String/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//count how many vowels are present in a string
+
+	str := "programming"
+	count := 0
+
+	for i := 0; i < len(str); i++ {
+		character := str[i]
+		if character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
