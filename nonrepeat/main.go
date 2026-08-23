8de03c24ce86820e42d0869314eda16d5fcaeadf
kashish srivastava <kashishsrivastava.delhi@gmail.com>
2026-08-23 23:45:34 +0530

    new change

--- nonrepeat/main.go
+++ nonrepeat/main.go
@@ -5,7 +5,19 @@ import "fmt"
 //Find the first character that repeats in a string.
 
 func main() {
-	str := "abcdab"
+	// str := "abcdab"
+	// seen := make(map[byte]bool)
+
+	// for i := 0; i < len(str); i++ {
+	// 	if seen[str[i]] == true {
+	// 		fmt.Printf("%c", str[i])
+	// 		break
+	// 	}
+	// 	seen[str[i]] = true
+	// }
+
+	str := "programming"
+
 	seen := make(map[byte]bool)
 
 	for i := 0; i < len(str); i++ {
